6558768f566520191264c52901f59a9613b4ccf0
Isabella Góes <141466024+isabellagoes@users.noreply.github.com>
2025-03-20 23:31:11 -0300

    Update Lista 2 - Arduino (4 ao 8).ino

--- Lista 2 - Arduino (4 ao 8).ino
+++ Lista 2 - Arduino (4 ao 8).ino
@@ -1,4 +1,150 @@
-// Exercicio 5
+//Exercício 1 – Acrescente mais um botão e um led a montagem. Modifique o programa para que cada botão acenda um dos led sem acionar o buzzer.
+int buttonPin = 2;
+int ledPin = 13; //led 1
+int buzzerPin = 7;
+
+int buttonPin2 = 6; //botão novo :)
+int ledPin2 = 12;   //led novo :)
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(buzzerPin, OUTPUT);
+
+  //novos pinos
+  pinMode(buttonPin2, INPUT);
+  pinMode(ledPin2, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  int buttonState = digitalRead(buttonPin);
+  int buttonState2 = digitalRead(buttonPin2);
+
+  Serial.println(buttonState);
+
+  if (buttonState == HIGH) {
+    digitalWrite(ledPin, LOW);
+  } else {
+    digitalWrite(ledPin, HIGH);
+  }
+
+  if (buttonState2 == LOW) {
+    digitalWrite(ledPin2, HIGH);
+    Serial.println("LED 2 aceso");
+  } else {
+    digitalWrite(ledPin2, LOW);
+  }
+
+  delay(500);
+}
+
+//Exercício 2 – Modifique o programa para o buzzer apitar quando o led 1 acender.
+int buttonPin = 2;
+int ledPin = 13;
+int buzzerPin = 7;
+int buttonState = 0;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(buzzerPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+
+  Serial.println(buttonState);
+
+  if (buttonState == HIGH) { //botão não pressionado
+    digitalWrite(ledPin, LOW);
+    noTone(buzzerPin); //desliga o buzzer
+  } else { //pressionado :)
+    digitalWrite(ledPin, HIGH);
+    tone(buzzerPin, 1000);
+  }
+
+  delay(500);  //estabilidade
+}
+
+
+//Exercício 3 – Modifique o programa para o buzzer apitar quando o led 2 acender.
+int buttonPin = 2;
+int ledPin = 13;
+int buzzerPin = 7;
+int buttonState = 0;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(buzzerPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+
+  Serial.println(buttonState);
+
+  if (buttonState == HIGH) { //botão não pressionado
+    digitalWrite(ledPin, LOW);
+    noTone(buzzerPin); //desliga o buzzer
+  } else { //pressionado :)
+    digitalWrite(ledPin, HIGH);
+    tone(buzzerPin, 1000);
+  }
+
+  delay(500);  //estabilidade
+}
+
+//Exercício 4 – Modifique o programa para o led 1 acender e o buzzer apitar quando o botão 2 é apertado e o led 2 acender e o buzzer apitar quando o botão 1 é apertado.
+int buttonPin1 = 2;
+int buttonPin2 = 6;
+int ledPin1 = 13;
+int ledPin2 = 12;
+int buzzerPin = 7;
+int buttonState1 = 0;
+int buttonState2 = 0;
+
+void setup() {
+  pinMode(buttonPin1, INPUT);
+  pinMode(buttonPin2, INPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  buttonState1 = digitalRead(buttonPin1);  // Lê o estado do botão 1
+  buttonState2 = digitalRead(buttonPin2);  // Lê o estado do botão 2
+
+  // Quando o botão 2 for pressionado, acende o LED 1 e o buzzer
+  if (buttonState2 == LOW) {  // Botão pressionado
+    digitalWrite(ledPin1, HIGH);  // Liga o LED 1
+    tone(buzzerPin, 1000);        // Liga o buzzer
+    Serial.println("Botão 2 pressionado - LED 1 aceso e buzzer tocando");
+  } else {
+    digitalWrite(ledPin1, LOW);   // Desliga o LED 1
+    noTone(buzzerPin);            // Desliga o buzzer
+  }
+
+  // Quando o botão 1 for pressionado, acende o LED 2 e o buzzer
+  if (buttonState1 == LOW) {  // Botão pressionado
+    digitalWrite(ledPin2, HIGH);  // Liga o LED 2
+    tone(buzzerPin, 1000);        // Liga o buzzer
+    Serial.println("Botão 1 pressionado - LED 2 aceso e buzzer tocando");
+  } else {
+    digitalWrite(ledPin2, LOW);   // Desliga o LED 2
+    noTone(buzzerPin);            // Desliga o buzzer
+  }
+
+}
+
+
+// Exercicio 5 – Escreva 2 funções que faça a leitura do estado de cada um dos botões.
 int botao1 = 2;
 int botao2 = 6;
 int EstadoBotao1 = 0;
@@ -36,7 +182,7 @@ void loop()
 }
 
 /*
-// Exercicio 6
+// Exercicio 6 – Escreva 2 funções que tomam a decisão se acende ou apaga cada um do led.
 int led1 = 13;
 int led2 = 12;
 int botao1 = 2;
@@ -93,7 +239,7 @@ void loop()
 */
 
 /*
-// Exercicio 7
+// Exercicio 7 – Escreva 1 função que tomam a decisão de apitar ou não o buzzer.
 int buzzer = 7;
 int botao1 = 2;
 int EstadoBotao1 = 0;
@@ -129,7 +275,41 @@ void loop()
 */
 
 /*
-// Exercicio 8 
+// Exercicio 8 – Copie o programa abaixo:
+Execute a música.
+Depois tire a função “delay(5000);” e coloque no programa o botão 1 para iniciar a música.
+#define DO 264
+#define RE 297
+#define MI 330
+#define FA 352
+#define SOL 396
+#define LA 440
+#define SI 495
+#define D 500
+#define N 29
+int notas[][2] = { {SOL, D/2}, {SOL, D/2}, {LA,D*2}, {SOL, D}, {DO*2,D},
+{SI,D*3}, {SOL, D/2}, {SOL, D/2}, {LA,D*2}, {SOL,D }, {RE*2,D }, {DO*2,D },
+{DO*2,D }, {0,D }, {MI*2,D/2 }, {MI*2,D/2 }, {SOL*2,D*2 }, {MI*2,D },
+{DO*2,D },
+{SI,D }, {LA,D }, {0,D }, {FA*2,D/2 }, {FA*2,D/2 }, {MI*2,D*2 }, {DO*2,D },
+{RE*2,D }, {DO*2,D }, {DO*2,D*3} };
+int duracao;
+int nota;
+int buzzerPin = 7;
+void setup(){
+pinMode(buzzerPin,OUTPUT);
+}
+void loop(){
+for (int i = 0; i < N; i++){
+nota = notas[i][0];
+duracao = notas[i][1];
+tone(buzzerPin, nota, duracao);
+delay(duracao);
+}
+delay(5000);
+}
+
+exercício:
 
 #define DO 264
 #define RE 297
