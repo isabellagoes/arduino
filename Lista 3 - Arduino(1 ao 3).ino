30f8a78b4a413cc1a5a471d1c52da7feac611c09
Isabella Góes <141466024+isabellagoes@users.noreply.github.com>
2025-03-21 07:26:45 -0300

    Update Lista 3 - Arduino(1 ao 3).ino

--- Lista 3 - Arduino(1 ao 3).ino
+++ Lista 3 - Arduino(1 ao 3).ino
@@ -131,9 +131,134 @@ void loop()
 */
 
 //Exercício 4 – Acrescente um botão a montagem. Faça funcionar os semáforos para travessia de pedestres. Um será para o carro e o outro para os pedestres.
+int pino8 = 8; //verde carro 1
+int pino9 = 9; //amarelo carro 1
+int pino10 = 10; //vermelho carro 1
+int pino11 = 11; //vermelho carro 2
+int pino12 = 12; //amarelo carro 2
+int pino13 = 13; //verde carro 2
+int botao = 7; // botão pedestre
+int pedestre_verde = 6; // verde pedestre
+int pedestre_vermelho = 5; // vermelho pedestre
 
+void setup() {
+   pinMode(pino8, OUTPUT);
+   pinMode(pino9, OUTPUT);
+   pinMode(pino10, OUTPUT);
+   pinMode(pino11, OUTPUT);
+   pinMode(pino12, OUTPUT);
+   pinMode(pino13, OUTPUT);
+   pinMode(pedestre_verde, OUTPUT);
+   pinMode(pedestre_vermelho, OUTPUT);
+   pinMode(botao, INPUT_PULLUP);
+}
+
+void loop() {
+   if (digitalRead(botao) == LOW) {
+       digitalWrite(pino8, LOW); //carro para
+       digitalWrite(pino9, HIGH);
+       delay(1000);
+       digitalWrite(pino9, LOW);
+       digitalWrite(pino10, HIGH);
+       digitalWrite(pedestre_vermelho, LOW);
+       digitalWrite(pedestre_verde, HIGH); //pode atravessar
+       delay(5000);
+       digitalWrite(pedestre_verde, LOW);
+       digitalWrite(pedestre_vermelho, HIGH);
+       digitalWrite(pino10, LOW);
+   }
+
+   //semáforo
+   digitalWrite(pino8, HIGH);
+   digitalWrite(pino10, LOW);
+   delay(4000);
+   digitalWrite(pino8, LOW);
+   digitalWrite(pino9, HIGH);
+   delay(1000);
+   digitalWrite(pino9, LOW);
+   digitalWrite(pino10, HIGH);
+}
 
 //Exercício 5 – Acrescente um buzzer a montagem e quando o botão for acionado no semáforo de pedestre o buzzer deve apitar por 100ms.
+int buzzer = 4;
 
+void setup() {
+   pinMode(pino8, OUTPUT);
+   pinMode(pino9, OUTPUT);
+   pinMode(pino10, OUTPUT);
+   pinMode(pino11, OUTPUT);
+   pinMode(pino12, OUTPUT);
+   pinMode(pino13, OUTPUT);
+   pinMode(pedestre_verde, OUTPUT);
+   pinMode(pedestre_vermelho, OUTPUT);
+   pinMode(botao, INPUT_PULLUP);
+   pinMode(buzzer, OUTPUT);
+}
+
+void loop() {
+   if (digitalRead(botao) == LOW) {
+       digitalWrite(pino8, LOW); //carros param
+       digitalWrite(pino9, HIGH);
+       delay(1000);
+       digitalWrite(pino9, LOW);
+       digitalWrite(pino10, HIGH);
+       digitalWrite(pedestre_vermelho, LOW);
+       digitalWrite(pedestre_verde, HIGH); //pode atravessar
+
+       //buzzer apita :(
+       digitalWrite(buzzer, HIGH);
+       delay(100);
+       digitalWrite(buzzer, LOW);
+
+       delay(5000);
+       digitalWrite(pedestre_verde, LOW);
+       digitalWrite(pedestre_vermelho, HIGH);
+       digitalWrite(pino10, LOW);
+   }
+
+   //semáforo
+   digitalWrite(pino8, HIGH);
+   digitalWrite(pino10, LOW);
+   delay(4000);
+   digitalWrite(pino8, LOW);
+   digitalWrite(pino9, HIGH);
+   delay(1000);
+   digitalWrite(pino9, LOW);
+   digitalWrite(pino10, HIGH);
+}
 
 //Exercício 6 – Transforme o acionamento do buzzer numa função.
+void tocarBuzzer(int tempo) {
+   digitalWrite(buzzer, HIGH);
+   delay(tempo);
+   digitalWrite(buzzer, LOW);
+}
+
+void loop() {
+   if (digitalRead(botao) == LOW) {
+       digitalWrite(pino8, LOW); //carros param
+       digitalWrite(pino9, HIGH);
+       delay(1000);
+       digitalWrite(pino9, LOW);
+       digitalWrite(pino10, HIGH);
+       digitalWrite(pedestre_vermelho, LOW);
+       digitalWrite(pedestre_verde, HIGH); // pode atravessar
+
+       tocarBuzzer(100);
+
+       delay(5000);
+       digitalWrite(pedestre_verde, LOW);
+       digitalWrite(pedestre_vermelho, HIGH);
+       digitalWrite(pino10, LOW);
+   }
+
+   //semáforo normal
+   digitalWrite(pino8, HIGH);
+   digitalWrite(pino10, LOW);
+   delay(4000);
+   digitalWrite(pino8, LOW);
+   digitalWrite(pino9, HIGH);
+   delay(1000);
+   digitalWrite(pino9, LOW);
+   digitalWrite(pino10, HIGH);
+}
